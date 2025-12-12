11b0e4981597fe5c04d3bb2c407b1c995f7b345c
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-12-12 16:03:42 +0700

    feat: add ErrorCode enum for standardized error handling

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -6,6 +6,7 @@ export * from './utils/assets.utils';
 export * from './utils/formatter.utils';
 export * from './utils/file.utils';
 export * from './utils/token.utils';
+export * from './utils/error-code.utils';
 
 // Types
 export * from './types';
